623e32251b2067fe48982b75acb4eafc0adaedd7
Leandro-Marques <Leandro@inw.com.br>
2023-02-08 00:12:25 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+// Importando o packages
+import express from 'express'
+import cors from 'cors'
+
+// instanciando o servidor
+const app = express()
+
+// configurando o servidor para receber requisições com o corpo no formato JSON
+app.use(express.json())
+
+// configurando o servidor para receber requisições qualquer origem
+app.use(cors())
+
+
+// importando os controllers
+import usuarioController from './controllers/usuarioController.js'
+import tarefaController from './controllers/tarefaController.js'
+
+usuarioController.rotas(app)
+tarefaController.rotas(app)
+
+export default app
